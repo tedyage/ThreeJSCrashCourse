4b7ad2b29e7c4164024623dd6f8c91285f85194d
tedyage <tedyage@sina.com>
2018-05-22 11:37:14 +0800

    update hologram.html

---
+++ js/ModelTouchControls.js
@@ -0,0 +1,167 @@
+var touch1PositionX=0,                    //第一个触碰X坐标
+    touch1PositionY=0,                    //第一个触碰Y坐标
+    touch2PositionX=0,                    //第二个触碰X坐标
+    touch2PositionY=0,                    //第二个触碰Y坐标
+    touchDistance=0;                     //两个触碰点间的距离
+var timeout;
+
+//自转物体方法
+var rotateModels = function(model,startRotation,deltaAngleX,deltaAngleY,speed){
+  //物体围绕x轴转动deltaAngleY/speed的角度
+  model.rotation.x = startRotation.x + deltaAngleX*speed;
+  //将物体的围绕X轴的转动角度限制在Math.PI/2和-Math.PI/2之间
+  if(model.rotation.x >= Math.PI/2){
+    model.rotation.x = Math.PI/2;
+  }else if(model.rotation.x <= -Math.PI/2){
+    model.rotation.x = -Math.PI/2;
+  }
+  //物体围绕y轴转动deltaAngleY/speed的角度
+  model.rotation.y = startRotation.y + deltaAngleY*speed;
+};
+
+//缩放物体方法
+var scaleModels = function(model,deltaScale){
+  model.scale.x = deltaScale;
+  model.scale.y = deltaScale;
+  model.scale.z = deltaScale;
+  if(model.scale.x <= 1.0){
+    model.scale.x = 1.0;
+    model.scale.y = 1.0;
+    model.scale.z = 1.0;
+  }else if(model.scale.x >= 1.8){
+    model.scale.x = 1.8;
+    model.scale.y = 1.8;
+    model.scale.z = 1.8;
+  }
+}
+
+//触碰开始方法
+var touchstart = function(event){
+  event.preventDefault();
+  //如果结束触碰后5秒之内，再次触碰，则清空倒计时
+  if(timeout>0)
+    clearTimeout(timeout);
+  touchDown = true;    //开始触碰
+  var targetTouches = event.targetTouches;
+  //排除无接触与三只手指以上接触的。
+  if(targetTouches === null|| targetTouches.length<=0||targetTouches.length>2)
+    return;
+  //单手指触碰，旋转物体
+  if(targetTouches.length==1){
+    //获取当前的触碰的位置x/y坐标
+    touch1PositionX = targetTouches[0].pageX;
+    touch1PositionY = targetTouches[0].pageY;
+    //获取当前的围绕x轴y轴的角度
+    for(var i = 0;i < 4;i++){
+      meshRotation[i] = mesh1.rotation;
+      cloudRotation[i] = cloud1.rotation;
+      lightBoxRotation[i] = lightBox1.rotation;
+    }
+  }
+  //两个手指触碰，缩放物体
+  else{
+    //获取当前的触碰的位置x/y坐标
+    touch1PositionX = targetTouches[0].pageX;
+    touch1PositionY = targetTouches[0].pageY;
+    touch2PositionX = targetTouches[1].pageX;
+    touch2PositionY = targetTouches[1].pageY;
+    touchDistance = Math.sqrt(Math.pow(touch2PositionX - touch1PositionX,2) + Math.pow(touch2PositionY - touch1PositionY,2));
+    //获取当前的缩放比例
+    for(var i = 0; i < 4; i++){
+      meshScale[i] = mesh1.scale.x;
+      cloudScale[i] = cloud1.scale.x;
+    }
+  }
+};
+
+//移动拖动手指
+var touchmove = function(event){
+  event.preventDefault();
+  //如果物体在运动中，则touchmove事件不作任何事情。
+  if(!touchDown)
+    return;
+  var targetTouches = event.targetTouches;
+  //排除无接触与三只手指以上接触的。
+  if(targetTouches === null || targetTouches.length<=0 || targetTouches.length>2)
+    return;
+  //单手指触碰，旋转物体
+  if(targetTouches.length==1){
+    //计算画布的宽和高
+    var canvasWidth = targetTouches[0].target.width;
+    var canvasHeight = targetTouches[0].target.height;
+    //计算每次移动在x轴，y轴的角度差
+    var deltaAngleX = (targetTouches[0].pageY - touch1PositionY)/canvasHeight * Math.PI;
+    var deltaAngleY = (targetTouches[0].pageX - touch1PositionX)/canvasWidth * Math.PI;
+    //转动物体
+    rotateModels(mesh1, meshRotation[0], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(mesh2, meshRotation[1], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(mesh3, meshRotation[2], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(mesh4, meshRotation[3], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(cloud1, cloudRotation[0], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(cloud2, cloudRotation[1], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(cloud3, cloudRotation[2], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(cloud4, cloudRotation[3], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(lightBox1, lightBoxRotation[0], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(lightBox2, lightBoxRotation[1], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(lightBox3, lightBoxRotation[2], deltaAngleX, deltaAngleY, 0.5);
+    rotateModels(lightBox4, lightBoxRotation[3], deltaAngleX, deltaAngleY, 0.5);
+    //覆盖当前触碰位置X/Y坐标，以备计算下次角度差
+    touch1PositionX = targetTouches[0].pageX;
+    touch1PositionY = targetTouches[0].pageY;
+    //覆盖当前的物体角度变量，以备计算下次角度差
+    for(var i = 0;i < 4;i++){
+      meshRotation[i] = mesh1.rotation;
+      cloudRotation[i] = cloud1.rotation;
+      lightBoxRotation[i] = lightBox1.rotation;
+    }
+  }
+  //两个手指触碰，缩放物体
+  else{
+    //计算当前两个触碰点间的距离与缩放比
+    var currentDistance = Math.sqrt(Math.pow(targetTouches[0].pageX - targetTouches[1].pageX,2) + Math.pow(targetTouches[0].pageY - targetTouches[1].pageY,2));
+    var currentScale1 = currentDistance/touchDistance*meshScale[0];
+    var currentScale2 = currentDistance/touchDistance*cloudScale[0];
+    //缩放物体
+    scaleModels(mesh1,meshScale[0]);
+    scaleModels(mesh2,meshScale[1]);
+    scaleModels(mesh3,meshScale[2]);
+    scaleModels(mesh4,meshScale[3]);
+    scaleModels(cloud1,cloudScale[0]);
+    scaleModels(cloud2,cloudScale[1]);
+    scaleModels(cloud3,cloudScale[2]);
+    scaleModels(cloud4,cloudScale[3]);
+    //重置触碰点之间的距离与缩放比，以备下次计算
+    touchDistance = currentDistance;
+    for(var i = 0; i < 4; i++){
+      meshScale[i] = mesh1.scale.x;
+      cloudScale[i] = cloud1.scale.x;
+    }
+    touch1PositionX = targetTouches[0].pageX;
+    touch1PositionY = targetTouches[0].pageY;
+    touch2PositionX = targetTouches[1].pageX;
+    touch2PositionY = targetTouches[1].pageY;
+  }
+
+};
+
+//完成触碰，手指离开
+var touchend = function(event){
+  event.preventDefault();
+  var touches = event.touches;
+  //排除无接触与三只手指以上接触的。
+  if(touches === null || touches.length<0 || touches.length>1)
+    return;
+  if(touches.length === 1){
+    touch1PositionX = touches[0].pageX;
+    touch1PositionY = touches[0].pageY;
+    return;
+  }
+  //恢复touchDown = false
+  timeout = setTimeout(function () {
+    touchDown = false;
+  }, 3000);
+}
+
+renderer1.domElement.addEventListener("touchstart",touchstart);
+renderer1.domElement.addEventListener("touchmove",touchmove);
+renderer1.domElement.addEventListener("touchend",touchend);
